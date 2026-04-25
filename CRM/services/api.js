0cd2192229c57eba36b2eed1f8b42e08294c904d
asishkumarv <asishkumarv@outlook.com>
2026-04-25 12:14:52 +0530

    backend error solving 2

---
+++ CRM/services/api.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://crm-system-wxnv.onrender.com/api",
+});
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const { register, login, approveUser } = require("../controllers/authController");
 
-router.post("/register", register);
-router.post("/login", login);
-router.put("/approve/:id", approveUser);
+// router.post("/register", register);
+// router.post("/login", login);
+// router.put("/approve/:id", approveUser);
 
-module.exports = router;
+module.exports = router; // ✅ MUST BE THIS
