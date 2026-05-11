13bd5c0652225dae810fa317e52383b47f500c0d
asishkumarv <asishkumarv@outlook.com>
2026-05-11 16:34:53 +0530

    email update

--- backend/controllers/leadController.js
+++ backend/controllers/leadController.js
@@ -4,20 +4,12 @@ const csv = require("csv-parser");
 const fs = require("fs");
 const nodemailer = require("nodemailer");
 const twilio = require("twilio");
-const dns = require("dns");
-
-dns.setDefaultResultOrder("ipv4first");
 // EMAIL CONFIG
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
   },
 });
 
